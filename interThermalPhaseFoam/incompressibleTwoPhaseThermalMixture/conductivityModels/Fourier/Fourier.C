872e5ddfec7f95b703adc4a4c3efc5dcfb7c1f67
MahdiNabil <nabil.mehdi1@gmail.com>
2015-07-13 16:07:22 -0400

    -a

---
+++ interThermalPhaseFoam/incompressibleTwoPhaseThermalMixture/conductivityModels/Fourier/Fourier.C
@@ -0,0 +1,88 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+	RAS analogy to the Newtonian viscosity model
+
+\*---------------------------------------------------------------------------*/
+
+#include "Fourier.H"
+#include "addToRunTimeSelectionTable.H"
+#include "surfaceFields.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace conductivityModels
+{
+    defineTypeNameAndDebug(Fourier, 0);
+    addToRunTimeSelectionTable(conductivityModel, Fourier, dictionary);
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::conductivityModels::Fourier::Fourier
+(
+    const word& name,
+    const dictionary& conductivityProperties,
+    const volVectorField& U,
+    const surfaceScalarField& phi
+)
+:
+    conductivityModel(name, conductivityProperties, U, phi),
+    lambda0_(conductivityProperties_.lookup("lambda")),
+    lambda_
+    (
+        IOobject
+        (
+            name,
+            U_.time().timeName(),
+            U_.db(),
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        U_.mesh(),
+        lambda0_
+    )
+{}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+bool Foam::conductivityModels::Fourier::read
+(
+    const dictionary& conductivityProperties
+)
+{
+    conductivityModel::read(conductivityProperties);
+
+    conductivityProperties_.lookup("lambda") >> lambda0_;
+    lambda_ = lambda0_;
+
+    return true;
+}
+
+
+// ************************************************************************* //
---
+++ interThermalPhaseFoam/incompressibleTwoPhaseThermalMixture/conductivityModels/conductivityModel/conductivityModelNew.C
@@ -0,0 +1,65 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "conductivityModel.H"
+#include "volFields.H"
+#include "surfaceFields.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+Foam::autoPtr<Foam::conductivityModel> Foam::conductivityModel::New
+(
+    const word& name,
+    const dictionary& conductivityProperties,
+    const volVectorField& U,
+    const surfaceScalarField& phi
+)
+{
+    const word modelType(conductivityProperties.lookup("thermalTransportModel")); //changed from transportModel for laminar fluids
+
+    Info<< "Selecting incompressible thermal transport model " << modelType << endl;
+
+    dictionaryConstructorTable::iterator cstrIter =
+        dictionaryConstructorTablePtr_->find(modelType);
+
+    if (cstrIter == dictionaryConstructorTablePtr_->end())
+    {
+        FatalErrorIn
+        (
+            "conductivityModel::New(const volVectorField&, "
+            "const surfaceScalarField&)"
+        )   << "Unknown thermalTransportModel type "
+            << modelType << nl << nl
+            << "Valid thermalTransportModels are : " << endl
+            << dictionaryConstructorTablePtr_->sortedToc()
+            << exit(FatalError);
+    }
+
+    return autoPtr<conductivityModel>
+        (cstrIter()(name, conductivityProperties, U, phi));
+}
+
+
+// ************************************************************************* //
