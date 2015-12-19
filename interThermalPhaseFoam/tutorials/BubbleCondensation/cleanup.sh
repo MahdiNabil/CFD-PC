9911c60e664a161892708cfd024783d1734d29b9
Alex Rattner <Alex.Rattner@gmail.com>
2015-12-19 17:54:35 -0500

    Wrapped up the bubble condensation tutorial case

--- interThermalPhaseFoam/tutorials/BubbleCondensation/cleanup.sh
+++ interThermalPhaseFoam/tutorials/BubbleCondensation/cleanup.sh
@@ -4,8 +4,12 @@
 rm -r processor*
 rm -r dynamicCode
 rm -r 0/*
+rm constant/polyMesh/cellZones
 rm constant/polyMesh/faces
+rm constant/polyMesh/faceZones
 rm constant/polyMesh/neighbour
 rm constant/polyMesh/owner
 rm constant/polyMesh/points
+rm constant/polyMesh/pointZones
+rm -r constant/polyMesh/sets
 rm Bubble_Condensation.dat
