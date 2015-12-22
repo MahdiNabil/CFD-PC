38c62b6248b042e961b204c4d503890dc46a9666
Alex Rattner <Alex.Rattner@gmail.com>
2015-12-22 18:14:04 -0500

    Cleaned up the wavy film condensation case a bit

--- interThermalPhaseFoam/tutorials/NusseltWavy/InitScript.sh
+++ interThermalPhaseFoam/tutorials/NusseltWavy/InitScript.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-m4 constant/polyMesh/blockMeshDict.m4 > constant/polyMesh/blockMeshDict
 blockMesh
 checkMesh
 mkdir 0
 cp -r A/* 0/ 
 setFields
 decomposePar -force
-mpirun -np 12 interThermalPhaseChangeFoam -parallel >NusseltWavy.log&
+mpirun -np 6 interThermalPhaseChangeFoam -parallel >NusseltWavy.log&
