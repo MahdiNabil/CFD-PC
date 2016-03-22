3a0d7adb7aee9981cae223f9edde349b16f0f653
Alex Rattner <Alex.Rattner@gmail.com>
2016-03-22 18:21:14 -0400

    Added improved transport weighting models at the interface, but it doesn't seem to make a significant effect

--- interThermalPhaseFoam/incompressibleTwoPhaseThermalMixture/twoPhaseThermalMixture.C
+++ interThermalPhaseFoam/incompressibleTwoPhaseThermalMixture/twoPhaseThermalMixture.C
@@ -2,8 +2,8 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
-     \\/     M anipulation  |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation and
+     \\/     M anipulation  | Copyright (C) 2016 Alex Rattner and Mahdi Nabil
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -208,10 +208,15 @@ Foam::twoPhaseThermalMixture::twoPhaseThermalMixture
 		U_.mesh(),
 		dimensionedScalar("rho", dimensionSet(1, -3, 0, 0, 0), 0),
 		calculatedFvPatchScalarField::typeName
-	)
+	),
+	mesh_(alpha1_.mesh() ),
+	ImprovedTransportBlending("no")
 
 {
-    calcNu();
+	//Read fluid properties, possibly switch on ImprovedTransportBlending
+	read();
+
+    	calcNu();
 	//I guess this is an initial calculation
 	calcLambda();
 	calcCp();
@@ -224,38 +229,106 @@ Foam::twoPhaseThermalMixture::twoPhaseThermalMixture
 Foam::tmp<Foam::volScalarField> Foam::twoPhaseThermalMixture::mu() const
 {
 	const volScalarField limitedAlpha1
-    (
-        min(max(alpha1_, scalar(0)), scalar(1))
-    );
-    return tmp<volScalarField>
-    (
-        //This is a pretty naive model for mixture viscosity (linear interpolation), but easy to evaluate
-	new volScalarField
-        (
-            "mu",
-			limitedAlpha1*rho1_*nuModel1_->nu()
-			+ (scalar(1) - limitedAlpha1)*rho2_*nuModel2_->nu()
-        )
-    );
+	(
+		min(max(alpha1_, scalar(0)), scalar(1))
+	);
+
+	//Improved transport blending Marschall et al. (2012) model
+	if (ImprovedTransportBlending)
+	{
+		//Proper interface blending for viscosity model:
+		//Cell face normals
+		const surfaceVectorField n_face = mesh_.Sf()/mesh_.magSf();
+		//Small unit of 1/L to avoid divide by zero issues
+		const dimensionedScalar eps_1_L("eps_1_L", dimless/dimLength, SMALL );
+		//Get the phase fraction cell face gradients
+		const surfaceScalarField alpha1f = fvc::interpolate( limitedAlpha1 );
+		const surfaceVectorField grad_alpha1f = fvc::interpolate( fvc::grad(limitedAlpha1) );
+		//Get the cell-face interface normal
+		const surfaceVectorField n_alpha1 = grad_alpha1f / ( mag( grad_alpha1f ) + eps_1_L );
+		//Weighting between serial and parallel blending of viscosity:
+		const surfaceScalarField eta_mu = mag( n_alpha1 & n_face );
+		const surfaceScalarField mu_ser = alpha1f*(rho1_*fvc::interpolate( nuModel1().nu() ) ) 
+		                             + (1.0 - alpha1f)*(rho2_*fvc::interpolate( nuModel2().nu() ) );
+		const surfaceScalarField mu_par = 1.0 /( alpha1f/(rho1_*fvc::interpolate( nuModel1().nu() ) ) 
+		                             + (1.0 - alpha1f)/(rho2_*fvc::interpolate( nuModel2().nu() ) ) );
+		const surfaceScalarField mu_impf = eta_mu*mu_ser + (1.0 - eta_mu)*mu_par;
+		
+		return tmp<volScalarField>
+		(
+			new volScalarField
+		        (
+				"mu",
+				 fvc::average(mu_impf)
+			)
+		);
+
+	}
+	else
+	{
+		return tmp<volScalarField>
+		(
+			new volScalarField
+		        (
+				"mu",
+				 limitedAlpha1*rho1_*nuModel1_->nu() 
+		                 + (scalar(1) - limitedAlpha1)*rho2_*nuModel2_->nu()
+			)
+		);
+	}
 }
 
 Foam::tmp<Foam::volScalarField> Foam::twoPhaseThermalMixture::lambda() const
 {
-    const volScalarField limitedAlpha1
-    (
-        min(max(alpha1_, scalar(0)), scalar(1))
-    );
+	const volScalarField limitedAlpha1
+	(
+		min(max(alpha1_, scalar(0)), scalar(1))
+	);
 
-    return tmp<volScalarField>
-    (
-        //This is a pretty naive model for mixture conductivity (linear interpolation), but easy to evaluate
-		new volScalarField
-        (
-            "lambda",
-            limitedAlpha1*lambdaModel1_->lambda()
-          + (scalar(1) - limitedAlpha1)*lambdaModel2_->lambda()
-        )
-    );
+	//Improved thermal transport blending
+	if (ImprovedTransportBlending)
+	{
+		//Proper interface blending for viscosity model:
+		//Cell face normals
+		const surfaceVectorField n_face = mesh_.Sf()/mesh_.magSf();
+		//Small unit of 1/L to avoid divide by zero issues
+		const dimensionedScalar eps_1_L("eps_1_L", dimless/dimLength, SMALL );
+		//Get the phase fraction cell face gradients
+		const surfaceScalarField alpha1f = fvc::interpolate( limitedAlpha1 );
+		const surfaceVectorField grad_alpha1f = fvc::interpolate( fvc::grad(limitedAlpha1) );
+		//Get the cell-face interface normal
+		const surfaceVectorField n_alpha1 = grad_alpha1f / ( mag( grad_alpha1f ) + eps_1_L );
+		//Weighting between serial and parallel blending of conductivity:
+		const surfaceScalarField eta_lambda = mag( n_alpha1 & n_face );
+		const surfaceScalarField lambda_ser = alpha1f * fvc::interpolate( lambdaModel1_->lambda() )
+		                             + (1.0 - alpha1f) * fvc::interpolate( lambdaModel2_->lambda() );
+		const surfaceScalarField lambda_par = 1.0 /( alpha1f / fvc::interpolate( lambdaModel1_->lambda() )
+		                             + (1.0 - alpha1f) / fvc::interpolate( lambdaModel2_->lambda() ) );
+		const surfaceScalarField lambda_impf = eta_lambda*lambda_par + (1.0 - eta_lambda)*lambda_ser;
+		return tmp<volScalarField>
+		(
+			new volScalarField
+		        (
+				"lambda",
+				 fvc::average(lambda_impf)
+			)
+		);
+
+	}
+	else
+	{
+		return tmp<volScalarField>
+		(
+			//This is a pretty naive model for mixture conductivity (linear interpolation), but easy to evaluate
+			new volScalarField
+			(
+				"lambda",
+				limitedAlpha1*lambdaModel1_->lambda()
+				+ (scalar(1) - limitedAlpha1)*lambdaModel2_->lambda()
+			)
+		);
+	}
+
 }
 
 Foam::tmp<Foam::volScalarField> Foam::twoPhaseThermalMixture::cp() const
@@ -315,38 +388,114 @@ Foam::tmp<Foam::volScalarField> Foam::twoPhaseThermalMixture::alpha() const
 
 Foam::tmp<Foam::surfaceScalarField> Foam::twoPhaseThermalMixture::muf() const
 {
-    const surfaceScalarField alpha1f
-    (
-        min(max(fvc::interpolate(alpha1_), scalar(0)), scalar(1))
-    );
+	const volScalarField limitedAlpha1
+	(
+		min(max(alpha1_, scalar(0)), scalar(1))
+	);
 
-    return tmp<surfaceScalarField>
-    (
-        new surfaceScalarField
-        (
-            "muf",
-            alpha1f*rho1_*fvc::interpolate(nuModel1_->nu())
-          + (scalar(1) - alpha1f)*rho2_*fvc::interpolate(nuModel2_->nu())
-        )
-    );
+	//Improved transport blending Marschall et al. (2012) model
+	if (ImprovedTransportBlending)
+	{
+		//Proper interface blending for viscosity model:
+		//Cell face normals
+		const surfaceVectorField n_face = mesh_.Sf()/mesh_.magSf();
+		//Small unit of 1/L to avoid divide by zero issues
+		const dimensionedScalar eps_1_L("eps_1_L", dimless/dimLength, SMALL );
+		//Get the phase fraction cell face gradients
+		const surfaceScalarField alpha1f = fvc::interpolate( limitedAlpha1 );
+		const surfaceVectorField grad_alpha1f = fvc::interpolate( fvc::grad(limitedAlpha1) );
+		//Get the cell-face interface normal
+		const surfaceVectorField n_alpha1 = grad_alpha1f / ( mag( grad_alpha1f ) + eps_1_L );
+		//Weighting between serial and parallel blending of viscosity:
+		const surfaceScalarField eta_mu = mag( n_alpha1 & n_face );
+		const surfaceScalarField mu_ser = alpha1f*(rho1_*fvc::interpolate( nuModel1().nu() ) ) 
+		                             + (1.0 - alpha1f)*(rho2_*fvc::interpolate( nuModel2().nu() ) );
+		const surfaceScalarField mu_par = 1.0 /( alpha1f/(rho1_*fvc::interpolate( nuModel1().nu() ) ) 
+		                             + (1.0 - alpha1f)/(rho2_*fvc::interpolate( nuModel2().nu() ) ) );
+		const surfaceScalarField mu_impf = eta_mu*mu_ser + (1.0 - eta_mu)*mu_par;
+				
+		return tmp<surfaceScalarField>
+		(
+			new surfaceScalarField
+		        (
+				"mu",
+				 mu_impf
+			)
+		);
+
+	}
+	else
+	{
+		const surfaceScalarField alpha1f = fvc::interpolate( limitedAlpha1 );
+		return tmp<surfaceScalarField>
+		(
+			new surfaceScalarField
+			(
+			    "mu",
+			    alpha1f*rho1_*fvc::interpolate(nuModel1_->nu())
+			  + (scalar(1) - alpha1f)*rho2_*fvc::interpolate(nuModel2_->nu())
+			)
+		);
+	}
 }
 
 Foam::tmp<Foam::surfaceScalarField> Foam::twoPhaseThermalMixture::lambdaf() const
 {
-    const surfaceScalarField alpha1f
-    (
-        min(max(fvc::interpolate(alpha1_), scalar(0)), scalar(1))
-    );
+	const volScalarField limitedAlpha1
+	(
+		min(max(alpha1_, scalar(0)), scalar(1))
+	);
 
-    return tmp<surfaceScalarField>
-    (
-        new surfaceScalarField
-        (
-            "lambdaf",
-            alpha1f*fvc::interpolate(lambdaModel1_->lambda())
-          + (scalar(1) - alpha1f)*fvc::interpolate(lambdaModel2_->lambda())
-        )
-    );
+
+	const surfaceScalarField alpha1f
+	(
+		fvc::interpolate(limitedAlpha1)
+	);
+
+
+
+	//Improved thermal transport blending
+	if (ImprovedTransportBlending)
+	{
+		//Proper interface blending for viscosity model:
+		//Cell face normals
+		const surfaceVectorField n_face = mesh_.Sf()/mesh_.magSf();
+		//Small unit of 1/L to avoid divide by zero issues
+		const dimensionedScalar eps_1_L("eps_1_L", dimless/dimLength, SMALL );
+		//Get the phase fraction cell face gradients
+		const surfaceVectorField grad_alpha1f = fvc::interpolate( fvc::grad(limitedAlpha1) );
+		//Get the cell-face interface normal
+		const surfaceVectorField n_alpha1 = grad_alpha1f / ( mag( grad_alpha1f ) + eps_1_L );
+		//Weighting between serial and parallel blending of conductivity:
+		const surfaceScalarField eta_lambda = mag( n_alpha1 & n_face );
+		const surfaceScalarField lambda_ser = alpha1f * fvc::interpolate( lambdaModel1_->lambda() )
+		                             + (1.0 - alpha1f) * fvc::interpolate( lambdaModel2_->lambda() );
+		const surfaceScalarField lambda_par = 1.0 /( alpha1f / fvc::interpolate( lambdaModel1_->lambda() )
+		                             + (1.0 - alpha1f) / fvc::interpolate( lambdaModel2_->lambda() ) );
+		const surfaceScalarField lambda_impf = eta_lambda*lambda_par + (1.0 - eta_lambda)*lambda_ser;
+		return tmp<surfaceScalarField>
+		(
+			new surfaceScalarField
+		        (
+				"lambda",
+				lambda_impf
+			)
+		);
+
+	}
+	else
+	{
+		return tmp<surfaceScalarField>
+		(
+			new surfaceScalarField
+			(
+				"lambdaf",
+				alpha1f*fvc::interpolate(lambdaModel1_->lambda())
+				+ (scalar(1) - alpha1f)*fvc::interpolate(lambdaModel2_->lambda())
+			)
+		);
+	}
+
 }
 
 Foam::tmp<Foam::surfaceScalarField> Foam::twoPhaseThermalMixture::alphaf() const
@@ -395,53 +544,37 @@ Foam::tmp<Foam::surfaceScalarField> Foam::twoPhaseThermalMixture::nuf() const
     );
 }
 
-/*
-Foam::tmp<Foam::volScalarField> Foam::twoPhaseThermalMixture::nuP(const label patchi) const
-{
-                //const fvMesh mesh;
-	        const fvPatchList& patches = mesh.boundary();		
-
-		forAll(patches, patchi)
-		{
-		const fvPatch& curPatch = patches[patchi];
-		  if (isA<wallFvPatch>(curPatch))
-		      {
-			//#include "checkPatchFieldTypes.H"
-			const scalarField& nu_ = nuP.boundaryField()[patchi];
-		      }
-		}
-	return nu_;
-}
-*/
 
 
 bool Foam::twoPhaseThermalMixture::read()
 {
-    if (transportModel::read())
-    {
-        if	//Check if the proper data is in the dictionaries
-        (
-			    nuModel1_().read(subDict(phase1Name_))
+	if (transportModel::read())
+	{
+		if	//Check if the proper data is in the dictionaries
+		(
+			nuModel1_().read(subDict(phase1Name_))
 			&& nuModel2_().read(subDict(phase2Name_))
 			&& lambdaModel1_().read(subDict(phase1Name_))
 			&& lambdaModel2_().read(subDict(phase2Name_))
-        )
-        {
-            nuModel1_->viscosityProperties().lookup("rho") >> rho1_;
-            nuModel2_->viscosityProperties().lookup("rho") >> rho2_;
+		)
+		{
+			nuModel1_->viscosityProperties().lookup("rho") >> rho1_;
+			nuModel2_->viscosityProperties().lookup("rho") >> rho2_;
 			subDict(phase1Name_).lookup("cp") >> cp1_;
 			subDict(phase2Name_).lookup("cp") >> cp2_;
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    else
-    {
-        return false;
-    }
+			readIfPresent("ImprovedTransportBlending", ImprovedTransportBlending);
+
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		return false;
+	}
 }
 
 
---
+++ interThermalPhaseFoam/tutorials/BubbleCondensationImprovedTransportModels/cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Cleanup script
+rm -r processor*
+rm -r dynamicCode
+rm -r 0/*
+rm constant/polyMesh/cellZones
+rm constant/polyMesh/faces
+rm constant/polyMesh/faceZones
+rm constant/polyMesh/neighbour
+rm constant/polyMesh/owner
+rm constant/polyMesh/points
+rm constant/polyMesh/pointZones
+rm -r constant/polyMesh/sets
+rm Bubble_Condensation.dat
